8cd1128623b3644048ff40ed6947abfe709afe56
Samandar <samandarnayimjonov@gmail.com>
2025-02-24 14:48:39 +0500

    Category model update

--- categories/models.py
+++ categories/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Category(models.Model):
+class Categories(models.Model):
     name = models.CharField(max_length=155)
     description = models.TextField()
 
--- categories/serializers.py
+++ categories/serializers.py
@@ -1,3 +1,6 @@
 from rest_framework import serializers
 
 
+class CategoryModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categ
