ed54693f8773438cb132fbc3f27cddc95af611ce
Samandar <samandarnayimjonov@gmail.com>
2025-02-24 14:17:51 +0500

    added a price field

--- products/models.py
+++ products/models.py
@@ -2,3 +2,6 @@ from django.db import models
 
 
 class Product(models.Model):
+    name = models.CharField(max_length=155)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
