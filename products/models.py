4012c9ba384bb55d6b704a845a8aaa796f0464ee
Samandar <samandarnayimjonov@gmail.com>
2025-02-24 14:20:35 +0500

    added category field ForeignKey

--- products/models.py
+++ products/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
+from categories.models import Category
+
 
 class Product(models.Model):
     name = models.CharField(max_length=155)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+
