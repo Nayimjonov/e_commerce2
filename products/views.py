c7d602ec31bb15aa45305e4c37694d1b7863d076
Samandar <samandarnayimjonov@gmail.com>
2025-02-24 15:31:20 +0500

    get and post done in views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Products
+from serializers import ProductModelSerializer
 
-# Create your views here.
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def product_list(request, pk=None):
+    if request.method == 'GET':
+        products = Products.objects.all()
+        serializer = ProductModelSerializer(products, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ProductModelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
