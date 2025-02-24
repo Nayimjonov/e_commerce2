8129144b74af73fa7480940d7adbd816756b182e
Samandar <samandarnayimjonov@gmail.com>
2025-02-24 15:18:42 +0500

    category relation in serializer

---
+++ products/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Products
+
+
+class ProductModelSerializer(serializers.ModelSerializer):
+    category =  serializers.StringRelatedField()
+    class Meta:
+        model = Products
+        fields = ('id', 'name', 'description', 'price', 'stock', 'category')
