9af34b183b2f7b5f6aa62d670c476027c7583255
Samandar <samandarnayimjonov@gmail.com>
2025-02-24 14:31:29 +0500

    field status update

--- orders/models.py
+++ orders/models.py
@@ -2,3 +2,15 @@ from django.db import models
 
 
 class Order(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('completed', 'Completed'),
+        ('cancelled', 'Cancelled'),
+    ]
+
+    customer_name = models.CharField(max_length=200)
+    customer_email = models.EmailField()
+    customer_phone = models.CharField(max_length=13)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
