d3ee23dce8e2058c69dc096faf01216423bb64db
Samandar <samandarnayimjonov@gmail.com>
2025-03-15 14:56:27 +0500

    update

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 from products.models import Products
 
 
@@ -26,3 +25,5 @@ class OrderItems(models.Model):
     quantity = models.PositiveIntegerField(default=1)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
+    def __str__(self):
+        return self.product.name
