0b9f3c05310d51b48d75a07719a4b60665b9905b
Samandar <samandarnayimjonov@gmail.com>
2025-03-15 15:18:25 +0500

    productga adminka qushildi

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Products
 
-# Register your models here.
+
+@admin.register(Products)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'stock', 'category')
+    search_fields = ('name', 'description')
+    list_filter = ('category',)
